3f5dcc7ddc3ad32a80cfa4c7234af940efc7df85
jose pacelli <josepacelli@gmail.com>
2026-07-17 09:30:17 -0300

    add RadioApplication with custom Coil configuration

---
+++ app/src/main/java/com/radioisaac/RadioApplication.kt
@@ -0,0 +1,25 @@
+package com.radioisaac
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+
+class RadioApplication : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader =
+        ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.20)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("station_logos"))
+                    .maxSizeBytes(50L * 1024 * 1024)
+                    .build()
+            }
+            .crossfade(true)
+            .build()
+}
